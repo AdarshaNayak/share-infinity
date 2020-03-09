c23a0959d7e2095bf5b83ead97f76907aa198ac5
Keshav <keshavupadhyayad@gmail.com>
2020-03-09 21:21:02 +0530

    added test script for dockerfile creation

--- src/app/local-server/local-server-app.js
+++ src/app/local-server/local-server-app.js
@@ -1,1 +1,21 @@
 // client-app which the user installs on his system
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/api/v1/local/dockerconfig", (req, res) => {
+	console.log("I am here!!");
+	console.log(req.body.commandsToRun);
+
+	res.send({
+		message: "Dockerfile create successfully"
+	});
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
