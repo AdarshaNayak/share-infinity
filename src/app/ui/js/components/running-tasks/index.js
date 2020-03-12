96e8c89f561b55302c7f306986fe2e6518168d01
adarsha.nayak <adarsha.nayak@curefit.com>
2020-03-12 20:01:46 +0530

    adding running tasks component

---
+++ src/app/ui/js/components/running-tasks/index.js
@@ -0,0 +1,8 @@
+import runningTasksTemplate from "./running-tasks.html";
+import runningTasksController from "./running-tasks-controller";
+
+export const runningTasksComponent = {
+    template: runningTasksTemplate,
+    controller: runningTasksController,
+    controllerAs: "$ctrl"
+};
