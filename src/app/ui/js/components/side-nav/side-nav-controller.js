692770ce70fc466fc332a282ef00883437d7c128
adarsha.nayak <adarsha.nayak@curefit.com>
2020-03-12 20:05:04 +0530

    running tasks component methods

--- src/app/ui/js/components/side-nav/side-nav-controller.js
+++ src/app/ui/js/components/side-nav/side-nav-controller.js
@@ -1,13 +1,17 @@
 export default class sideNavController {
 	static get $inject() {
-		return ["$timeout", "$mdSidenav", "$log", "$location"];
+		return ["$timeout", "$mdSidenav", "$log", "$location","$http"];
 	}
 
-	constructor($timeout, $mdSidenav, $log, $location) {
+	constructor($timeout, $mdSidenav, $log, $location,$http) {
 		this.$mdSidenav = $mdSidenav;
 		this.$log = $log;
 		this.$location = $location;
 		this.toggleLeft = this.buildToggler("left");
+		this.$http = $http;
+		this.localIp = "http://localhost:3000";
+		this.currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+		this.userId = this.currentUser.userId;
 	}
 
 	buildToggler(componentId) {
@@ -25,5 +29,28 @@ export default class sideNavController {
 	goToSubmittedTasks() {
 		this.$location.path("/submitted-tasks");
 		this.toggleLeft();
+	}
+	goToRunningTasks() {
+		this.$location.path("/running-tasks");
+		this.toggleLeft();
+	}
+
+	poll(mode) {
+		console.log(mode);
+		if(mode === undefined || mode === false){
+			this.$http.post(this.ip + "/api/v1/tasks", {
+				userId: userId,
+				providerId: providerId
+			});
+			this.$http.get(
+				this.localIp+"/api/v1/local/polling/provider/"+this.userId+"/start"
+			);
+		}
+		else{
+			this.$http.get(
+				this.localIp+"/api/v1/local/polling/provider/"+this.userId+"/stop"
+			);
+		}
+
 	}
 }
