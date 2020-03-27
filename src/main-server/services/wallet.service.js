de571f5fe53ed0920a3e9429e2057e13560af04e
adarsha.nayak <adarsha.nayak@curefit.com>
2020-03-27 21:34:57 +0530

    payment status

--- src/main-server/services/wallet.service.js
+++ src/main-server/services/wallet.service.js
@@ -46,6 +46,7 @@ async function makePayment({transactionId,amount}) {
                 await consumerWallet.save();
                 const task = await Task.findOne({transactionId:transactionId});
                 task.isPaymentDone = true;
+                await task.save();
             }
             catch (e) {
                 console.log("error occurred while updating balance ",e);
