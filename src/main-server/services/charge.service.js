c959898cc767e3eea476390dba8c9da85887af2a
adarsha.nayak <adarsha.nayak@curefit.com>
2020-03-28 17:49:59 +0530

    charge service completed

---
+++ src/main-server/services/charge.service.js
@@ -0,0 +1,22 @@
+const db = require("../_helpers/db");
+const Provider = db.Provider;
+
+async function getProviderCharge(providerId) {
+    const provider = await Provider.findOne({providerId:providerId});
+    console.log("provider doc ",provider);
+    return { "charge": provider.providerCharge }
+}
+
+async function updateProviderCharge({userId,amount}) {
+    amount = parseInt(amount);
+    const provider = await Provider.findOne({providerId:userId});
+    provider.providerCharge = amount;
+    await provider.save();
+    console.log("provider charge updated ",provider);
+    return { "charge": provider.providerCharge }
+}
+
+module.exports = {
+    getProviderCharge,
+    updateProviderCharge
+}
